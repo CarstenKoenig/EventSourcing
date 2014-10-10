d2fd0150d2fe053664761ec3372ae8b07c9ce4e5
Carsten König <Carsten@gettingsharper.de>
2014-10-10 09:48:43 +0200

    build works on linux

--- buildMono.sh
+++ buildMono.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mono --runtime=v4.0 tools/NuGet/nuget.exe install FAKE -OutputDirectory tools -ExcludeVersion
+mono --runtime=v4.0 tools/NuGet/NuGet.exe install FAKE -OutputDirectory tools -ExcludeVersion
 mono --runtime=v4.0 tools/FAKE/tools/FAKE.exe buildMono.fsx $@
